be692bac587e2a30a38c050a7735c4e9c7bbd4a2
hanisa1 <hanisamohamed1@gmail.com>
2020-10-11 11:27:54 +1100

    Showing stage and initial setup

---
+++ src/controller/StoreController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import javafx.beans.property.*;
+import java.io.*;
+import java.text.*;
+import au.edu.uts.ap.javafx.*;
+import model.*;
+
+public class StoreController {
+}
